a075ea4ffbf43eea3a3a1da87f6257a857294a5e
João Gabriel Fontes Rezende <rezende.f.gabriel@gmail.com>
2020-11-30 22:55:54 -0300

    Adds functions to the Projects subcommand and starts using the EzConsole extension

--- whatsmyprogress/Commands/Projects.cs
+++ whatsmyprogress/Commands/Projects.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using whatsmyprogress.DAL.Entities;
 using whatsmyprogress.Repository;
 using whatsmyprogress.Repository.Interfaces;
+using Console = EzConsole.EzConsole;
 
 namespace whatsmyprogress.Commands
 {
@@ -34,36 +35,57 @@ namespace whatsmyprogress.Commands
             catch (Exception ex)
             {
                 Console.WriteLine("Sorry, the system failed to recover the list of projects");
-                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
             }
         }
 
         [Command(Description = "Adds a new project")]
-        public void Add(string name) {
+        public void Add(string title) {
             try
             {
-                _projectRepository.Add(new Project { Title = name });
-                Console.WriteLine($"{name} was added to the list of projects!");
+                _projectRepository.Add(new Project { Title = title }).Wait();
+                Console.WriteLine($"{title} was added to the list of projects!", ConsoleColor.Green);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Sorry, the system failed to add to the list of projects");
-                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
             }
         }
 
-        [Command(Description = "Deletes a project")]
+        [Command(Description = "Renames a specific project")]
+        public void Rename(int id, string newTitle)
+        {
+            try
+            {
+                var specificProject = _projectRepository.Get(id).Result;
+                var oldTitle = specificProject.Title;
+
+                specificProject.Title = newTitle;
+
+                _projectRepository.Update(specificProject).Wait();
+
+                Console.WriteLine($"{oldTitle} is now kwon as {newTitle}!", ConsoleColor.Green);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, the system failed to delete from the list of projects");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        [Command(Description = "Deletes a specific project")]
         public void Delete(int id)
         {
             try
             {
-                _projectRepository.Delete(id);
+                _projectRepository.Delete(id).Wait();
                 Console.WriteLine($"The project was successfuly deleted");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Sorry, the system failed to delete from the list of projects");
-                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
             }
         }
     }
